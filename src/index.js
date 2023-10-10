d675c5490d056b7485076fc500205cf82f147bf3
Adolf <adolfnyerere@gmail.com>
2023-10-10 06:07:55 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,43 +1,21 @@
 const init = () => {
-    const input = document.querySelector('input');
-    
-    input.addEventListener('input', (event) => {
-        const value = event.target.value;
-        const result = document.querySelector('.result');
-        result.innerHTML = value;
-    }
-
-    );
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => {
-        const ul = document.querySelector('ul');
-        json.forEach(user => {
-            const li = document.createElement('li');
-            li.innerHTML = user.name;
-            ul.appendChild(li);
-        });
-    });
-
-    const button = document.querySelector('button');
-    button.addEventListener('click', () => {
-        const ul = document.querySelector('ul');
-        ul.innerHTML = '';
-    });
-
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const input = document.querySelector('input');
-        const ul = document.querySelector('ul');
-        const li = document.createElement('li');
-        li.innerHTML = input.value;
-        ul.appendChild(li);
-        input.value = '';
-    });
-
+    const inputForm = document.querySelector('form')
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+  
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
